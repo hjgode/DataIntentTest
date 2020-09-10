8801511476403e46b6b55bc232ca23f8b50e93e9
hjgode <hjgode@gmail.com>
2020-09-10 13:47:42 +0200

    added more details about intent in logview

--- app/src/main/java/com/sample/dataintenttest/MainActivity.java
+++ app/src/main/java/com/sample/dataintenttest/MainActivity.java
@@ -2,7 +2,9 @@ package com.sample.dataintenttest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Notification;
 import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
@@ -11,6 +13,9 @@ import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.Locale;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
     final static String TAG="DataIntentTest";
@@ -49,11 +54,34 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onReceive(Context context, Intent intent) {
                 addLog("===================\nreceived Intent:");
+                Log.d(TAG, intent.toURI());
                 Bundle bundle = intent.getExtras();
+                Bundle bundleExtra = intent.getBundleExtra("extrakey");
+                String action = intent.getAction();
+                Set<String> categories = intent.getCategories();
+                String packet = intent.getPackage();
+                ComponentName o = intent.getComponent();
+                String classname="";
+                if(o!=null) {
+                    classname = o.getClassName();
+                }
                 if (bundle != null) {
+                    addLog( "action="+action);
+                    if(categories!=null) {
+                        if (categories.size() > 0) {
+                            for (String c : categories) {
+                                Log.d(TAG, "category: " + c);
+                                addLog("category: " + c);
+                            }
+                        }
+                    }else{
+                        addLog("categories=null");
+                    }
+                    addLog( "package="+packet);
+                    addLog("classname="+classname);
+
                     for (String key : bundle.keySet()) {
                         Object value = bundle.get(key);
-                        Log.d(TAG, String.format("'%s': %s (%s)", key, value.toString(), value.getClass().getName()));
                         addLog(String.format("'%s': %s (%s)", key, value.toString(), value.getClass().getName()));
                     }
                 }
@@ -72,7 +100,7 @@ public class MainActivity extends AppCompatActivity {
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                Log.d("IntentTest", msg);
+                Log.d(TAG, msg);
                 String old=txtLog.getText().toString();
                 old+="\n"+msg;
                 txtLog.setText(old);
